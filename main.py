0bc25828a02c9369779abcacb1d36a5896ae93ed
Ridwan <ridwibra@gmail.com>
2024-09-21 16:50:04 +0300

    Add CORS middleware to allow all origins

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 import spacy
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Download the model at runtime
 spacy.cli.download("en_core_web_sm")
 
